0685f09da97c9cd7603bbed468aab13969fa9c0e
Malek <malek.mousa1407@gmail.com>
2021-10-23 21:47:46 +0400

    Fixed file formatting.

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
-require('dotenv').config();
-const path = require('path');
+const express = require('express')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-const app = express();
+const app = express()
 
 // Rate limiting
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutes
     max: 100
-});
-app.use(limiter);
-app.set('trust proxy', 1);
+})
+
+// Middleware
+app.use(limiter)
 app.use(express.static('public'))
+app.set('trust proxy', 1)
 
 // Routes
-app.use('/api', require('./routes/weather.js'));
+app.use('/api', require('./routes/weather.js'))
 
 // Enable CORS
 app.use(cors())
 
 // Initializing the server
 app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
